3e4d6f3ca97edf5d0b63fb9d968a9c27136912c1
deds129 <nik.chudinov129@mail.ru>
2021-05-19 14:10:12 +0300

    day02ex01 start + create plan

---
+++ src/com/company/day02/ex01/Program.java
@@ -0,0 +1,29 @@
+package com.company.day02.ex01;
+/*
+Now you need to implement an application that will determine the level of similarity
+between texts. The simplest and most obvious method to do this is to analyze the
+frequency of occurrence of the same words.
+
+Your goal is to implement an application that accepts two files as an input (both files are
+passed as command-line arguments) and displays their similarity comparison outcome
+(cosine measure).
+The program shall also create dictionary.txt file containing a dictionary based on these
+files.
+ */
+
+/* todo:
+    1)read files + read words (from terminal)
+    2)create dictionary to save all word from files
+    2.1) save dictionary to file(dictionary.txt)
+    3)create vectors to save word frequency
+    4)count similarity using formula
+    5)output result
+
+ */
+
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
