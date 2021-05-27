6e48b1db88648e5461fd87f52228946a41c82928
deds129 <nik.chudinov129@mail.ru>
2021-05-27 22:25:35 +0300

    Start day03 (created flag validator)

--- src/com/company/day02/ex02/Program.java
+++ src/com/company/day02/ex02/Program.java
@@ -8,43 +8,13 @@ import java.util.Objects;
 import java.util.Scanner;
 
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
-/*
-$ java Program --current-folder=C:/MAIN
-C:/MAIN
--> ls
-folder1 60 KB
-folder2 90 KB
--> cd folder1
-C:/MAIN/folder1
--> ls
-image.jpg 10 KB
-animation.gif 50 KB
--> mv image.jpg image2.jpg
--> ls
-image2.jpg 10 KB
-animation.gif 50 KB
--> mv animation.gif ../ folder2
--> ls
-image2.jpg 10 KB
--> cd ../ folder2
-C:/MAIN/folder2
--> ls
-text. txt 10 KB
-Program.java 80 KB
-animation.gif 50 KB
--> exit
-
- //Program --current-folder=C:/
- */
 public class Program {
     public static void main(String[] args) {
-        //check flags
-
-//        if (!flagChecker(args))
-//            return ;
+        if (!flagChecker(args))
+            return ;
         try {
-//            String rootPath = args[0].substring(args[0].indexOf('=') + 1);
-            startFromRootPath("C:/");
+            String rootPath = args[0].substring(args[0].indexOf('=') + 1);
+            startFromRootPath(rootPath);
 
         }catch (Exception e)
         {
@@ -53,7 +23,6 @@ public class Program {
 
     }
 
-    //todo: replace func using Path, Paths, Files methods(Java 8), not File(Java 7)
     public static void startFromRootPath(String rootPath) throws IOException {
         File rootFile = new File(rootPath);
         if (rootFile.isDirectory())
@@ -103,14 +72,13 @@ public class Program {
 
 
         if (Files.exists(Paths.get(fileName1))) {
-            //throw DirectoryNotEmptyException
+            //throws DirectoryNotEmptyException
             testFile1 = Files.move(testFile1, Paths.get(mvFolder), REPLACE_EXISTING);
             System.out.println(mvFolder);
         }
 
 
     }
-    //todo: replace func using Path, Paths, Files methods(Java 8), not File(Java 7)
     public static void showDirContent(File dir) {
         if (dir.isDirectory()) {
             for (File item : Objects.requireNonNull(dir.listFiles())) {
@@ -128,12 +96,10 @@ public class Program {
 
     public static boolean flagChecker(String[] args)
     {
-        //too many args
         if (args.length != 1) {
             System.err.println("Invalid number of arguments passed");
             return false;
         }
-        //wrong file
         if (!(args[0].startsWith("--current-folder="))) {
             System.err.println("Invalid flag");
             return false;
