541a81e970f1175cc939ece853e4d70ba6fcf974
deds129 <nik.chudinov129@mail.ru>
2021-06-02 23:38:12 +0300

    start ex02

---
+++ src/com/company/day03/ex02/Program.java
@@ -0,0 +1,19 @@
+package com.company.day03.ex02;
+/*
+Let’s assume there is an array of integer values. Your goal is to calculate the sum of array
+elements using several "summing" threads. Each thread computes a certain section inside
+the array. The number of elements in each section is constant, except for the last one (its
+size can differ upward or downward).
+The array shall be randomly generated each time. Array length and the number of threads
+are passed as command-line arguments.
+To make sure the program operates correctly, we should start by calculating the sum of
+array elements using a standard method.
+Maximum number of array elements is 2,000,000. Maximum number of threads is no
+greater than current number of array elements. Maximum modulo value of each array
+element is 1,000. All data is guaranteed to be valid.
+ */
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
